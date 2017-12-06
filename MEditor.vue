d3c3e81d6e30292801a97aa96e4b74b6e39f8b90
misaka <misaka@github.com>
2017-12-06 19:32:17 +0800

    first creat

---
+++ MEditor.vue
@@ -0,0 +1,78 @@
+/***
+*  @author misaka
+*  @date 2017/12/6
+*/
+<style>
+    .m-span:hover{
+        background-color: lightgoldenrodyellow;
+    }
+</style>
+
+<template>
+    <Tooltip content="单击编辑" v-if="!edit" @click.native="edit=true" placement="top">
+        <span class="m-span"><slot></slot></span>
+    </Tooltip>
+    <Input v-else-if="type=='input'" v-model="currentValue" :type="inputType" @keydown.native.enter="close" :autofocus="true" title="敲击回车确认输入"/>
+    <Select
+            v-else-if="type=='select'"
+            v-model="currentSelectValue"
+            :label-in-value="true"
+            :filterable="filterable"
+            :multiple="multiple"
+            :loading="loading"
+            @on-query-change = 'onQuery'
+            @on-change="closeSelect">
+        <slot name="option"></slot>
+    </Select>
+</template>
+
+<script>
+    export default {
+        props:{
+            value:{
+                type:[String,Number,Object,Array],
+                required:true
+            },
+            type:{
+                type:String,
+                validator(value){
+                    return ['input','select'].indexOf(value)>-1;
+                },
+                default:'input'
+            },
+            inputType:{
+                type:String,
+                default:'text'
+            },
+            filterable:{
+                type:Boolean,
+                default:false
+            },
+            multiple:Boolean,
+            loading:{
+                type:Boolean,
+                default:false
+            }
+        },
+        data(){
+            return {
+                edit:false,
+                currentValue:this.value,
+                currentSelectValue:this.value
+            };
+        },
+        methods:{
+            close(){
+                this.edit = false;
+                this.$emit('input',this.currentValue);
+            },
+            closeSelect(value){
+                this.edit = false;
+                this.$emit('input',value);
+            },
+            onQuery(value){
+                this.$emit('on-query',value);
+            }
+        }
+    };
+</script>
---
+++ readme.md
@@ -0,0 +1,77 @@
+# 点击编辑器
+>依赖于iview + Vue
+
+**input**
+
+***props***
+
+ 属性| 说明 | 类型 | 默认值 | 是否必须 
+ --- | -----| -----| -------| ---------
+ value | 输入框的值，可以使用v-model双向绑定 | String、Number| 无| 是
+ type | 更改数据时的类型，可选值为`input`、`select` | String | input | 否
+ inputType | 输入框的类型，可选值为`text`、`password`、`textarea`、`url`、`email`、`date` | String | text | 否
+ 
+ ***event***
+ 
+  无
+ 
+ **selectType**
+ 
+ ***props***
+ 
+ 属性| 说明 | 类型 | 默认值 | 是否必须 
+  --- | -----| -----| -------| ---------
+   value | select | Number、String、Array| 无| 是
+  filterable | 是否可以搜索 | boolean | false | 否
+  multiple | 是否可以多选 | boolean | false | 否
+  loading | 是否显示加载中 | boolean | false | 否
+  
+   ***event***
+   
+   事件名| 说明 | 返回值
+   --- | -----| -----
+   on-query | 搜索词变化时触发 | query
+   
+   **slot**
+   
+   名称 | 说明
+   ---|---
+   无 | 默认显示的文本
+   option | 选择器的选项
+     
+   ##使用：
+   ```
+<template>
+      <MEditor v-model="text" type="select" style="width: 500px" :filterable="true" @on-query="query" :multiple="true">
+            {{text}}
+            <template slot="option">
+                <Option :value="123">selectable</Option>
+                <Option :value="333">ff</Option>
+            </template>
+        </MEditor>
+        
+        <MEditor v-model="text" type="input" style="width: 500px">
+        {{text}}
+        </MEditor>
+        
+</template>
+<script>
+     import MEditor from './MEditor.vue';
+        export default {
+            components:{
+                MEditor
+            },
+            data(){
+                return {
+                    text:[]
+                };
+            },
+            methods:{
+                query(value){
+                   ///remote query
+                }
+            }
+        };
+</script>
+```
+   
